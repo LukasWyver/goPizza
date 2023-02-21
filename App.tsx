6e2d0f251f9880eb93baa02595544cb309310d53
Lucas Gimenes <lucas_sin5@hotmail.com>
2023-02-21 15:49:48 -0300

    settings expo-google-fonts

--- App.tsx
+++ App.tsx
@@ -1,22 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import {Teste} from '@screens/Teste'
+import { View } from 'react-native';
+import * as SplashScreen from 'expo-splash-screen';
+import { useFonts, DMSans_400Regular } from '@expo-google-fonts/dm-sans';
+import { DMSerifDisplay_400Regular } from '@expo-google-fonts/dm-serif-display';
 
 export default function App() {
+  SplashScreen.preventAutoHideAsync();
+
+  const [fontsLoaded] = useFonts({
+    DMSans_400Regular,
+    DMSerifDisplay_400Regular
+  });
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
+  SplashScreen.hideAsync();
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
