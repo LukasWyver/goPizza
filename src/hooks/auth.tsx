f10c36b80940d4b2d362715a68811c3321ec9a77
Lucas Gimenes <lucas_sin5@hotmail.com>
2023-02-22 11:50:05 -0300

    feat: ForgotPassword user

--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -10,7 +10,7 @@ import auth from '@react-native-firebase/auth';
 import firestore from '@react-native-firebase/firestore';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
-type User = { 
+type User = {
   id: string;
   name: string;
   isAdmin: boolean;
@@ -19,9 +19,10 @@ type User = {
 type AuthContextData = {
   signIn: (email: string, password: string) => Promise<void>;
   signOut: () => Promise<void>;
+  forgotPassword: (email: string) => Promise<void>;
   isLogging: boolean;
   user: User | null;
- }
+}
 
 type AuthProviderProps = {
   children: ReactNode;
@@ -35,54 +36,54 @@ function AuthProvider({ children }: AuthProviderProps) {
   const [user, setUser] = useState<User | null>(null);
   const [isLogging, setIsLogging] = useState(false);
 
-  async function signIn(email: string, password: string){
-    if(!email || !password){
+  async function signIn(email: string, password: string) {
+    if (!email || !password) {
       return Alert.alert('Login', 'Informe o e-mail e a senha.')
     }
 
     setIsLogging(true);
 
     auth()
-    .signInWithEmailAndPassword(email, password)
-    .then(account => {
-      firestore()
-      .collection('users')
-      .doc(account.user.uid)
-      .get()
-      .then(async (profile) => {
-        const { name, isAdmin } = profile.data() as User;
+      .signInWithEmailAndPassword(email, password)
+      .then(account => {
+        firestore()
+          .collection('users')
+          .doc(account.user.uid)
+          .get()
+          .then(async (profile) => {
+            const { name, isAdmin } = profile.data() as User;
 
-        if(profile.exists){
-          const userData = {
-            id: account.user.uid,
-            name,
-            isAdmin,
-          };
-          
-          await AsyncStorage.setItem(USER_COLLECTION, JSON.stringify(userData));
-          setUser(userData);
+            if (profile.exists) {
+              const userData = {
+                id: account.user.uid,
+                name,
+                isAdmin,
+              };
+
+              await AsyncStorage.setItem(USER_COLLECTION, JSON.stringify(userData));
+              setUser(userData);
+            }
+          })
+          .catch(() => Alert.alert('Login', 'Não foi possível buscar os dados de perfil do usuário.'));
+      })
+      .catch(error => {
+        const { code } = error;
+
+        if (code === 'auth/user-not-found' || code === 'auth/wrong-password') {
+          return Alert.alert('Login', 'E-mail e/ou senha inválida.');
+        } else {
+          return Alert.alert('Login', 'Não foi possível realizar o login.');
         }
       })
-      .catch(() => Alert.alert('Login', 'Não foi possível buscar os dados de perfil do usuário.'));
-    })
-    .catch(error => {
-      const { code } = error;
-
-      if(code === 'auth/user-not-found' || code === 'auth/wrong-password'){
-        return Alert.alert('Login', 'E-mail e/ou senha inválida.');
-      } else {
-        return Alert.alert('Login', 'Não foi possível realizar o login.');
-      }
-    })
-    .finally(() => setIsLogging(false))
+      .finally(() => setIsLogging(false))
   }
 
-  async function loadUserStorageData(){
+  async function loadUserStorageData() {
     setIsLogging(true);
 
     const storedUser = await AsyncStorage.getItem(USER_COLLECTION);
 
-    if(storedUser){
+    if (storedUser) {
       const userData = JSON.parse(storedUser) as User;
       console.log(userData)
       setUser(userData);
@@ -91,10 +92,21 @@ function AuthProvider({ children }: AuthProviderProps) {
     setIsLogging(false);
   }
 
-  async function signOut(){
+  async function signOut() {
     await auth().signOut();
     await AsyncStorage.removeItem(USER_COLLECTION);
     setUser(null);
+  }
+
+  async function forgotPassword(email: string) {
+    if (!email) {
+      return Alert.alert('Redefinir senha', 'Informe o e-mail.');
+    }
+
+    auth()
+      .sendPasswordResetEmail(email)
+      .then(() => Alert.alert('Redefinir senha', 'Enviamos um link no seu e-mail para redefinir sua senha.'))
+      .catch(() => Alert.alert('Redefinir senha', 'Não foi possível enviar o e-mail para redefinir sua senha.'))
   }
 
   useEffect(() => {
@@ -102,13 +114,19 @@ function AuthProvider({ children }: AuthProviderProps) {
   }, [])
 
   return (
-    <AuthContext.Provider value={{signIn, signOut, isLogging, user}}>
+    <AuthContext.Provider value={{
+      signIn,
+      signOut,
+      forgotPassword,
+      isLogging,
+      user
+    }}>
       {children}
     </AuthContext.Provider>
   )
 }
 
-function useAuth(){
+function useAuth() {
   const context = useContext(AuthContext);
 
   return context;
